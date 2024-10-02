b55aac83960823a421e18266c01a83196d168c86
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-10-02 17:41:46 +0100

    create empty spring boot project

---
+++ src/main/java/com/github/joe_mccarthy/metmesh/MetmeshApplication.java
@@ -0,0 +1,13 @@
+package com.github.joe_mccarthy.metmesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetmeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetmeshApplication.class, args);
+	}
+
+}
